e517cd25fa43ab6afdff50e6dd2f6f27ee2207b1
Benjamin Phillips <phillib4@sunyit.edu>
2019-01-18 16:52:35 -0500

    Added stateful dialog in order to use a DropDownButton. Dialog opens a new route to edit note types.

--- lib/main.dart
+++ lib/main.dart
@@ -46,77 +46,9 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class NoteType {
-  String name;
-  List<Field> fields;
-
-  NoteType(this.name, this.fields);
-}
-
-
-
-class Field {
-  String _type;
-  String _s;
-  double _n;
-  bool _b;
-
-  Field.number(this._n){
-   _type = 'num';
-  }
-
-  Field.string(this._s){
-    _type = 'str';
-  }
-
-  Field.boolean(this._b){
-    _type = 'bool';
-  }
-
-  @override
-  String toString() {
-    if (_type == 'num'){
-      return _n.toString();
-    } else if (_type == 'str'){
-      return _s;
-    } else if (_type == 'bool'){
-      return _b.toString();
-    } else {
-      return 'Found a bad field';
-    }
-  }
-}
-
 class _MyHomePageState extends State<MyHomePage> {
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/types.dat');
-  }
-
-  Future<File> writeTypes(List<NoteType> types) async {
-    final file = await _localFile;
-
-    //Write the file
-    return file.writeAsString(types.toString());
-  }
-
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
-
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -124,23 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
@@ -149,68 +65,102 @@ class _MyHomePageState extends State<MyHomePage> {
                 'create new',
               ),
             ),
-//            Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
             FloatingActionButton(
               child: Icon(Icons.add),
-              onPressed: _openNewItemDialog,
-            )
+              onPressed: () {
+                showDialog(context: context,
+                    builder: (BuildContext context) => new MyDialog());
+              },
+            ),
           ],
         ),
       ),
     );
   }
+}
 
-  void _openNewItemDialog() {
-    print("Pressed Plus");
-    _newNoteDialog();
-  }
+class MyDialog extends StatefulWidget {
+  const MyDialog();
 
-  Future<void> _newNoteDialog() async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: true, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('add a new note'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: TextField(
-                    decoration: new InputDecoration(hintText: 'title'),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child:  DropdownButton(
-                      items: null,
-                      onChanged: null,
-                      hint: Text('type'),
-                  ),
-                ),
-                RaisedButton(
-                  child: Text('create new type'),
-                  onPressed: _openNewTypeDialog,
-                )
-              ],
+  @override
+  State createState() => new MyDialogState();
+}
+
+class MyDialogState extends State<MyDialog> {
+  String _selectedId;
+
+  Widget build(BuildContext context) {
+    return new SimpleDialog(
+      title: new Text('add new note'),
+
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: TextField(
+              decoration: new InputDecoration(hintText: 'give it a title'),
             ),
           ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('create'),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: new DropdownButton<String>(
+              hint: const Text('pick a type'),
+              value: _selectedId,
+              onChanged: (String value) {
+                setState(() {
+                  _selectedId = value;
+                });
+              },
+              items: <String>['simple note', 'titled note'].map((String value) {
+                return new DropdownMenuItem<String>(
+                  value: value,
+                  child: new Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 48),
+            child: RaisedButton(
+              child: Text('create new type'),
               onPressed: () {
-                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TypeEditPage()),
+                );
               },
             ),
-          ],
-        );
-      },
+          ),
+
+          new FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: new Text('create'),
+          ),
+        ],
+    );
+  }
+}
+
+class TypeEditPage extends StatefulWidget{
+  @override
+  _TypeEditPageState createState() => _TypeEditPageState();
+}
+
+class _TypeEditPageState extends State<TypeEditPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('type creation'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+
+        ],
+      ),
     );
   }
 
-  void _openNewTypeDialog() {}
 }
